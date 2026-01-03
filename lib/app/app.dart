14b014cd7b9782403bf9ef9430b736a763efe6bb
srian2 <sandeepkandel45@gmail.com>
2026-01-03 08:06:17 +0545

    routes modified

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,12 @@
 // lib/app.dart
 import 'package:flutter/material.dart';
 import 'package:nepalexplorer/app/theme/theme_data.dart';
+import 'package:nepalexplorer/features/dashboard/presentation/pages/dashboard_screen.dart';
 import 'package:nepalexplorer/features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'package:nepalexplorer/features/splash/presentation/pages/splash_screen.dart';
+import 'package:nepalexplorer/features/auth/presentation/pages/login_screen.dart';
+import 'package:nepalexplorer/features/auth/presentation/pages/register_screen.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,8 +18,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: getApplicationTheme(),
       home: const SplashScreen(),
-        routes: {
-        '/onboarding': (context) => const OnboardingScreen(), // <- define route
+      routes: {
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const DashboardScreen(),
       },
     );
   }
